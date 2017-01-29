83a3155adf049ebfd2c9919668f1dd8f92ab70af
Paula Ceccon <paulaceccon@Paulas-iMac.local>
2017-01-29 15:36:53 -0200

    Small adjustment.

--- VBO_VBA/QuadShaderProgram.cpp
+++ VBO_VBA/QuadShaderProgram.cpp
@@ -53,7 +53,7 @@ void QuadShaderProgram::BuildModel()
 	_modelCoordinates = { 1.0f,  1.0f, 0.0f,
 		                  1.0f, -1.0f, 0.0f,
 		                 -1.0f,  1.0f, 0.0f,
-						 -1.0f, 1.0f, 0.0f,
+						 -1.0f,  1.0f, 0.0f,
 						  1.0f, -1.0f, 0.0f,
 						 -1.0f, -1.0f, 0.0f };
 
